c58787100d355854d8938d6b465e192707daf0b5
hubo416 <hubo416@naver.com>
2025-06-18 18:49:32 +0900

    BOJ 13549.cpp
    틀림..

---
+++ WEEK02/MJ/13549.cpp
@@ -0,0 +1,56 @@
+#include <stack>
+#include <queue>
+#include <vector>
+#include <utility>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+
+
+int MAX = 100000;
+
+int visit[200002];
+int cnt = 0;
+
+
+int main(void) {
+
+	int Subin, Dongsang;
+	cin >> Subin >> Dongsang;
+	queue<int> Q;
+
+	visit[Subin] = 1; // (0, 0)을 방문했다고 명시
+	Q.push(Subin); // 큐에 시작점인 (0, 0)을 삽입.
+	while (!Q.empty()) {
+		int cur = Q.front(); Q.pop();
+		if (cur == Dongsang) {
+			cout << visit[cur] - 1;
+			return 0;
+		}
+		for (int dir = 0; dir < 3; dir++) { // 상하좌우 칸을 살펴볼 것이다.
+			int nx;
+			switch (dir) {
+				case 0:
+					nx = cur * 2;
+					break;
+				case 1:
+					nx = cur + 1;
+					break;
+				case 2:
+					nx = cur - 1;
+					break;
+			}
+			if (nx < 0 || nx > MAX) continue; 
+			if (visit[nx]) continue; 
+
+			if (dir) visit[nx] = visit[cur] + 1;
+			else visit[nx] = visit[cur];
+			Q.push(nx);
+			for (int i = 1; i <= 20; i++) {
+			//cout << visit[i] << " ";
+			}
+			//cout << "\n";
+		}
+	}
+}
