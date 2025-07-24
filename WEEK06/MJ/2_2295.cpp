62121da50b3b3dd15f80c187c40c87d27e46da46
hubo416 <hubo416@naver.com>
2025-07-24 17:18:42 +0900

    .

--- WEEK06/MJ/2_2295.cpp
+++ WEEK06/MJ/2_2295.cpp
@@ -24,14 +24,14 @@ int main(void) {
     sort(arr, arr + N);
     for (int u = 0; u < N; u++) {
         for (int v = u; v < N; v++) {
-            two.push_back(arr[u] + arr[v]);
+            two.push_back(arr[u] + arr[v]); // x, y, z 를 더해서 k 를 만들기 위해 x 와 y 를 더한 원소들을 two 에 저장
         }
     }
     sort(two.begin(), two.end());
     for (int u = 0; u < N; u++) {
         for (int v = 0; v < N; v++) {
-            if (binary_search(two.begin(), two.end(), arr[v] - arr[u]) && arr[v] > max_value) {
-                max_value = arr[v];
+            if (binary_search(two.begin(), two.end(), arr[v] - arr[u]) && arr[v] > max_value) { // x+y+z=k 인 경우와 k-z=x+z 인 경우가 같음.
+                max_value = arr[v];                                                             // k-z=x+z 일때 k값을 max_value에 갱신함.
             }
         }
     }
