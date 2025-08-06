0e9551e7e3bef6f706d8e78f8b975cf4065a80ce
hubo416 <hubo416@naver.com>
2025-08-06 19:11:37 +0900

    Update G_17835.cpp

--- WEEK08/MJ/G_17835.cpp
+++ WEEK08/MJ/G_17835.cpp
@@ -8,7 +8,6 @@ using namespace std;
 vector <pair<int, int>> adj[100003];
 priority_queue <pair<long long, int>> pq;
 long long dist[100003];
-long long result[100003];
 long long INF = 1e18;
 int N, M, K, a, b, c;
 int temp;
@@ -17,7 +16,7 @@ long long max_dist;
 
 void PRINT() {
     for (int i = 1; i <= N; i++) {
-        cout << result[i] << ' ';
+        cout << dist[i] << ' ';
     }
     cout << '\n';
 }
@@ -31,35 +30,31 @@ int main(void) {
         adj[b].push_back({ c, a });
     }
 
-    fill(result, result + N + 1, INF);
     for (int u = 0; u < K; u++) {
         cin >> temp;
-        fill(dist, dist + N + 1, INF);
-        pq.push({ 0, temp });
-        dist[temp] = 0;
-        while (!pq.empty()) {
-            long long cur_dist = -pq.top().Dist;
-            int cur_idx = pq.top().Idx;
-            pq.pop();
-            if (dist[cur_idx] != cur_dist) continue;
-            for (auto nxt : adj[cur_idx]) {
-                if (dist[nxt.Idx] <= dist[cur_idx] + nxt.Dist) continue;
-                dist[nxt.Idx] = dist[cur_idx] + nxt.Dist;
-                pq.push({ -dist[nxt.Idx], nxt.Idx });
-            }
+        adj[0].push_back({ 1, temp });
+    }
+    fill(dist, dist + N + 1, INF);
+    pq.push({ 0, 0 });
+    dist[0] = 0;
+    while (!pq.empty()) {
+        long long cur_dist = -pq.top().Dist;
+        int cur_idx = pq.top().Idx;
+        pq.pop();
+        if (dist[cur_idx] != cur_dist) continue;
+        for (auto nxt : adj[cur_idx]) {
+            if (dist[nxt.Idx] <= dist[cur_idx] + nxt.Dist) continue;
+            dist[nxt.Idx] = dist[cur_idx] + nxt.Dist;
+            pq.push({ -dist[nxt.Idx], nxt.Idx });
         }
-
-        for (int v = 1; v <= N; v++) {
-            if (result[v] > dist[v]) result[v] = dist[v];
-        }
-        //PRINT();
     }
     for (int v = 1; v <= N; v++) {
-        if (result[v] > max_dist) {
-            max_dist = result[v];
+        if (dist[v] > max_dist) {
+            max_dist = dist[v];
             max_idx = v;
         }
     }
-    cout << max_idx << '\n' << max_dist;
+    
+    cout << max_idx << '\n' << max_dist - 1;
     return 0;
 }
