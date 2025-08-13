9461c5cca99da5b62a37f82c3c8edb6760cca749
hubo416 <hubo416@naver.com>
2025-08-13 16:08:17 +0900

    Update C_20040.cpp

--- WEEK09/MJ/C_20040.cpp
+++ WEEK09/MJ/C_20040.cpp
@@ -24,7 +24,8 @@ int main(void) {
     for (int u = 0; u < N; u++) v.push_back(-1);
     for (int u = 1; u <= M; u++) {
         cin >> a >> b;
-        if (Find(a) == Find(b)) {
+        if (Find(a) == Find(b)) { // Find(a) == Find(b) 일때 사이클이 처음 생김
+                                  // a와 b는 이미 같은 그룹이므로 이 둘을 이으면 사이클
             cout << u;
             return 0;
         }
