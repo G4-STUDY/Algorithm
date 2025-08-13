347a0f33d43eb067685f1f4047a5f0fc19f19698
hubo416 <hubo416@naver.com>
2025-08-13 14:50:59 +0900

    Create C_20040.cpp

---
+++ WEEK09/MJ/C_20040.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector <int> v;
+int N, M, a, b;
+
+int Find(int x) {
+    if (v[x] < 0) return x;
+    return v[x] = Find(v[x]);
+}
+
+bool Union(int i, int j) {
+    i = Find(i);
+    j = Find(j);
+    if (i == j) return false;
+    v[j] = i;
+    return true;
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int u = 0; u < N; u++) v.push_back(-1);
+    for (int u = 1; u <= M; u++) {
+        cin >> a >> b;
+        if (Find(a) == Find(b)) {
+            cout << u;
+            return 0;
+        }
+        Union(a, b);
+    }
+    cout << 0;
+    return 0;
+}
