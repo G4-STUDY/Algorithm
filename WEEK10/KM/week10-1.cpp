20d7c426ee8917477084561e8704f8e52564ea70
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2025-08-25 16:22:08 +0900

    week11-km

--- WEEK10/KM/week10-1.cpp
+++ WEEK10/KM/week10-1.cpp
@@ -232,3 +232,20 @@ void printInput(int n, const vector<array<int, 4>> &graph, int r, const vector<i
 2 -3 2 -1
 
  */
+
+
+
+
+ /*
+ 3 4 -1 4 5
+ 출발 1
+    6
+ 1-2-4--3-5
+ 1 2 3 4
+
+ 1,3
+ 1,5
+
+ 1 - 3
+
+ */
