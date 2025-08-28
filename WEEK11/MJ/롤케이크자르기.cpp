b815204805fe862ed29ee939410f9e3d954eff57
hubo416 <hubo416@naver.com>
2025-08-28 17:25:22 +0900

    .

--- WEEK11/MJ/롤케이크자르기.cpp
+++ WEEK11/MJ/롤케이크자르기.cpp
@@ -18,7 +18,10 @@ int solution(vector<int> topping) {
         m2[topping[u]]++;
         m1[topping[u]]--;
         if(m1[topping[u]] == 0) m1.erase(topping[u]);
-        if (m1.size() == m2.size()) answer++;
+        
+        // 이때 m2.size()는 왼쪽 부분의 토핑 개수, m1.size()는 오른쪽 부분의 토핑 개수임 
+        
+        if (m1.size() == m2.size()) answer++; // 두 부분의 토핑 개수가 같을 때 answer값에 1을 더함
     }
     return answer;
 }
