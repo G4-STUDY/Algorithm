c73ff3e51abffd5ea288a87b5d99cc566f910568
hubo416 <hubo416@naver.com>
2025-08-28 17:43:52 +0900

    .

--- WEEK11/MJ/시소짝꿍.cpp
+++ WEEK11/MJ/시소짝꿍.cpp
@@ -11,11 +11,13 @@ long long solution(vector<int> weights) {
     int weight = 0;
     for (int u = 0; u < weights.size(); u++) {
         weight = weights[u];
-        answer += upper_bound(weights.begin() + u + 1, weights.end(), weight) - lower_bound(weights.begin() + u + 1, weights.end(), weight);
-        for (int v = 2; v <= 4; v++) {
-            for (int w = 2; w <= 4; w++) {
-                if (v == w) continue;
-                if (weight * v % w) continue;
+
+        // 양쪽의 무게가 같을때 짝꿍이 몇쌍 있는지 세기
+        answer += upper_bound(weights.begin() + u + 1, weights.end(), weight) - lower_bound(weights.begin() + u + 1, weights.end(), weight); 
+        for (int v = 2; v <= 4; v++) {           //--------------------------------------------
+            for (int w = 2; w <= 4; w++) {       // 양쪽의 무게가 다를 때 균형이 맞는
+                if (v == w) continue;            // 짝꿍이 몇쌍 있는지 세기
+                if (weight * v % w) continue;    //--------------------------------------------
                 answer += upper_bound(weights.begin() + u + 1, weights.end(), weight*v/w) - lower_bound(weights.begin() + u + 1, weights.end(), weight*v/w);
             }
         }
