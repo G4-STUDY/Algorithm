e858848a4b24e3a09351638c35c3406025203881
Joos <jjssgg226@gmail.com>
2025-08-28 17:46:05 +0900

    주성 시소짝궁 업데이트

---
+++ WEEK11/JS/시소 짝궁.cpp
@@ -0,0 +1,71 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+int binarySearch(int st, int en, int target,const vector<int>& weights );
+    
+long long solution(vector<int> weights) {
+    long long answer = 0;
+    
+    sort(weights.begin(), weights.end());
+    for(int i = 0; i < weights.size() - 1; i++) 
+    {
+        int cur_weight = weights[i];
+        auto upper_it
+        auto lower_it
+            
+        if(cur_weight % 3 == 0)
+        {
+            
+            int target = (cur_weight / 3) * 4;
+            upper_it = upper_bound(weights.begin() + i + 1, weights.end(), target);
+            lower_it = lower_bound(weights.begin() + i + 1, weights.end(), target);
+            answer += upper_it - lower_it;
+        }
+        
+        if(cur_weight % 2 == 0)
+        {
+            int target = (cur_weight / 2) * 3;
+            upper_it = upper_bound(weights.begin() + i + 1, weights.end(), target);
+            lower_it = lower_bound(weights.begin() + i + 1, weights.end(), target);
+            answer += upper_it - lower_it;
+        }
+        
+       
+        upper_it = upper_bound(weights.begin() + i + 1, weights.end(), cur_weight);
+        lower_it = lower_bound(weights.begin() + i + 1, weights.end(), cur_weight);
+        answer += upper_it - lower_it;
+        
+        
+        
+        upper_it = upper_bound(weights.begin() + i + 1, weights.end(), cur_weight * 2);
+        lower_it = lower_bound(weights.begin() + i + 1, weights.end(), cur_weight * 2);
+        answer += upper_it - lower_it;
+        
+        
+    }
+  
+    return answer;
+}
+ 
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
