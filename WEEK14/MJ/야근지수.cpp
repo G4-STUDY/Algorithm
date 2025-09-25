314f44271723a48621ceade95263bf646f632c16
hubo416 <hubo416@naver.com>
2025-09-25 17:47:20 +0900

    Create 야근지수.cpp

---
+++ WEEK14/MJ/야근지수.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+priority_queue <int> pq;
+
+long long solution(int n, vector<int> works) {
+    long long answer = 0;
+    while(!works.empty()) {
+        pq.push(works.back());
+        works.pop_back();
+    }
+    
+    int temp = 0;
+    for (int u = 0; u < n; u++) {
+        if (pq.top() == 0) break;
+        temp = pq.top();
+        pq.pop();
+        temp--;
+        pq.push(temp);
+    }
+    while(!pq.empty()) {
+        answer += pq.top()*pq.top();
+        pq.pop();
+    }
+    return answer;
+}
+
