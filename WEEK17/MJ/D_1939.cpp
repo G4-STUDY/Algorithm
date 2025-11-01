5c1918ff893af94b0cfe332453ae8caa2378dddc
hubo416 <hubo416@naver.com>
2025-11-01 18:00:22 +0900

    .

--- WEEK17/MJ/D_1939.cpp
+++ WEEK17/MJ/D_1939.cpp
@@ -10,7 +10,7 @@ using namespace std;
 priority_queue <pair <int, int>> pq;
 
 vector <pair <int, int>> adj[10003];
-long long vis[10003];
+int vis[10003];
 
 int N, M;
 int a, b, c;
@@ -18,6 +18,8 @@ int st, en;
 int result = 2000000000;
 
 int main() {
+    ios::sync_with_stdio(false);
+	cin.tie(0);
     cin >> N >> M;
     for (int u = 0; u < M; u++) {
         cin >> a >> b >> c;
@@ -27,19 +29,20 @@ int main() {
     
     cin >> st >> en;
     pq.push({2000000000, st});
-    vis[st] = 1;
+    vis[st] = 2000000000;
     while (!pq.empty()) {
         int cur_node = pq.top().Node;
         int cur_cost = pq.top().Cost;
         pq.pop();
-        if (cur_cost < result) result = cur_cost;
+        if (vis[cur_node] != 1 && cur_cost < result) result = cur_cost;
+        vis[cur_node] = 1;
+        //cout << cur_node << ' ' << cur_cost << '\n';
         if (cur_node == en) {
             cout << result;
             return 0;
         }
         for (auto nxt : adj[cur_node]) {
-            if (vis[nxt.Node] >= vis[cur_node] + nxt.Cost) continue;
-            vis[nxt.Node] = vis[cur_node] + nxt.Cost;
+            if (vis[nxt.Node] == 1) continue;
             pq.push({nxt.Cost, nxt.Node});
         }
     }
