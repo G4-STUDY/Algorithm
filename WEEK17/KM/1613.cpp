7800c6abf0345c9ff31d016bfeae2cef57d95e91
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2025-11-04 18:37:10 +0900

    kmupdate17

--- WEEK17/KM/1613.cpp
+++ WEEK17/KM/1613.cpp
@@ -1,40 +1,41 @@
 #include <iostream>
 
 using namespace std;
-int n;
-int arr[100001];
-int dp[100001];
-int m;
+
+int n,m;
+bool edge[401][401];
+int s; 
 int main()
 {
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int a,b;
+        cin>>a>>b;
+        edge[a][b]=true;
     }
-    for(int i=0;i<n;i++){
-        dp[i]=100001;
-    }
-    dp[0]=arr[0];
-    m=0;
-    for(int i=1;i<n;i++){
-        if(arr[i]>dp[m]){//증가하는경우
-            dp[++m]=arr[i];
-        }
-        else{//작은경우
-            int l=0;
-            int r=m+1;
-            while(l<r){
-                int mid=(l+r)/2;
-                if(dp[mid]<arr[i]){
-                    l=mid+1;
-                }
-                else{
-                    r=mid;
-                }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            for(int k=1;k<=n;k++){
+                if(edge[j][i]&&edge[i][k])
+                    edge[j][k]=true;
             }
-            dp[l]=arr[i];
         }
     }
-    cout<<n-m-1;
+    cin>>s;
+    for(int i=0;i<s;i++){
+        int a,b;
+        cin>>a>>b;
+        if(edge[a][b]){
+            cout<<-1<<'\n';
+        }
+        else if(edge[b][a]){
+            cout<<1<<'\n';
+        }
+        else{
+            cout<<0<<'\n';
+        }
+    }
     return 0;
 }
